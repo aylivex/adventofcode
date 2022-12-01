7f3ff2fb05bf1b02432fab8e647212223f1b2e2f
Alexey Ivanov <code@alexey-ivanov.name>
2022-12-01 22:34:43 +0000

    Day 01 - First task

--- src/Day01.java
+++ src/Day01.java
@@ -1,5 +1,28 @@
-public class Day01 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Day01 {
+    public static void main(String[] args) throws IOException {
+        int max = 0;
+        int curr = 0;
+        try (final BufferedReader r = new BufferedReader(
+                new InputStreamReader(Day01.class.getResourceAsStream("day01.input.txt")))) {
+            String line;
+            while ((line = r.readLine()) != null) {
+                if (line.isEmpty()) {
+                    if (curr > max) {
+                        max = curr;
+                    }
+                    curr = 0;
+                } else {
+                    curr += Integer.parseInt(line);
+                }
+            }
+            if (curr > max) {
+                max = curr;
+            }
+            System.out.println(max);
+        }
     }
 }
