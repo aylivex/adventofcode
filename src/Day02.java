ec61ea15a4a4060fc174258fc625959789be2580
Alexey Ivanov <code@alexey-ivanov.name>
2022-12-02 19:42:38 +0000

    Day 02 - Second task

--- src/Day02.java
+++ src/Day02.java
@@ -6,25 +6,28 @@ public class Day02 {
     public static void main(String[] args) throws IOException {
         try (final BufferedReader r = new BufferedReader(
                 new FileReader("input/day02.input.txt"))) {
-            int score = 0;
+            int score1 = 0;
+            int score2 = 0;
             String line;
             while ((line = r.readLine()) != null) {
-                score += getScore(line);
+                score1 += getScore1(line);
+                score2 += getScore2(line);
             }
-            System.out.println(score);
+            System.out.println(score1);
+            System.out.println(score2);
         }
     }
 
-    private static int getScore(String line) {
+    private static int getScore1(String line) {
         return getChoiceScore(line.charAt(2))
                + getWinScore(line.charAt(0), line.charAt(2));
     }
 
     private static int getChoiceScore(char you) {
         return switch (you) {
-            case 'X' -> 1;
-            case 'Y' -> 2;
-            case 'Z' -> 3;
+            case 'X', 'A' -> 1;
+            case 'Y', 'B' -> 2;
+            case 'Z', 'C' -> 3;
             default  -> throw new IllegalArgumentException(
                     "Illegal character '" + you + "'");
         };
@@ -36,6 +39,31 @@ public class Day02 {
             case 'Y' -> (opp == 'A') ? 6 : (opp == 'B' ? 3 : 0);
             case 'Z' -> (opp == 'B') ? 6 : (opp == 'C' ? 3 : 0);
             default  -> throw new IllegalArgumentException(
+                    "Illegal character '" + you + "'");
+        };
+    }
+
+    private static int getScore2(String line) {
+        return getYourWinScore(line.charAt(2))
+               + getChoiceScore(mapYourChoice(line.charAt(0), line.charAt(2)));
+    }
+
+    private static char mapYourChoice(char opp, char you) {
+        return switch (you) {
+            case 'X' -> (opp == 'A' ? 'C' : (opp == 'B' ? 'A' : 'B'));
+            case 'Y' -> opp;
+            case 'Z' -> (opp == 'A' ? 'B' : (opp == 'B' ? 'C' : 'A'));
+            default -> throw new IllegalArgumentException(
+                    "Illegal character '" + you + "'");
+        };
+    }
+
+    private static int getYourWinScore(char you) {
+        return switch (you) {
+            case 'X' -> 0;
+            case 'Y' -> 3;
+            case 'Z' -> 6;
+            default -> throw new IllegalArgumentException(
                     "Illegal character '" + you + "'");
         };
     }
