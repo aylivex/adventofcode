2aeb27e1be009d1794dbc70d3ecf595c04e3c958
Alexey Ivanov <code@alexey-ivanov.name>
2022-12-03 19:43:22 +0000

    Day03 stub

---
+++ src/Day03.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day03 {
+    public static void main(String[] args) throws IOException {
+        try (final BufferedReader r = new BufferedReader(
+                new FileReader("input/day03.input.txt"))) {
+            String line;
+            while ((line = r.readLine()) != null) {
+            }
+        }
+
+    }
+}
