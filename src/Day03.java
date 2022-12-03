e257572f66f5c2a8ecd86e2dcde4d21a98e6a82f
Alexey Ivanov <code@alexey-ivanov.name>
2022-12-03 20:23:44 +0000

    Day 03 - First task

--- src/Day03.java
+++ src/Day03.java
@@ -7,8 +7,20 @@ public class Day03 {
         try (final BufferedReader r = new BufferedReader(
                 new FileReader("input/day03.input.txt"))) {
             String line;
+            int sum = 0;
             while ((line = r.readLine()) != null) {
+                final int half = line.length() / 2;
+                final String left = line.substring(0, half);
+                final String right = line.substring(half);
+                sum += right.chars()
+                            .filter(c -> left.indexOf(c) >= 0)
+                            .findFirst()
+                            .stream()
+                            .map(c -> c <= 'Z' ? 27 + (c - 'A')
+                                               :  1 + (c - 'a'))
+                            .sum();
             }
+            System.out.println(sum);
         }
 
     }
