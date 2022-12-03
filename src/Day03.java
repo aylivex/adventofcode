4f4bb7775d1480eb74b02d08add46b45aa432261
Alexey Ivanov <code@alexey-ivanov.name>
2022-12-03 20:47:23 +0000

    Day 03 - Second task

--- src/Day03.java
+++ src/Day03.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Day03 {
     public static void main(String[] args) throws IOException {
@@ -8,17 +11,22 @@ public class Day03 {
                 new FileReader("input/day03.input.txt"))) {
             String line;
             int sum = 0;
+            final List<String> group = new ArrayList<>(3);
             while ((line = r.readLine()) != null) {
-                final int half = line.length() / 2;
-                final String left = line.substring(0, half);
-                final String right = line.substring(half);
-                sum += right.chars()
-                            .filter(c -> left.indexOf(c) >= 0)
-                            .findFirst()
-                            .stream()
-                            .map(c -> c <= 'Z' ? 27 + (c - 'A')
-                                               :  1 + (c - 'a'))
-                            .sum();
+                group.add(line);
+                if (group.size() == 3) {
+                    group.sort(Comparator.comparingInt(String::length));
+                    sum += group.get(2)
+                                .chars()
+                                .filter(c -> group.get(0).indexOf(c) >= 0
+                                             && group.get(1).indexOf(c) >= 0)
+                                .findFirst()
+                                .stream()
+                                .map(c -> c <= 'Z' ? 27 + (c - 'A')
+                                                   :  1 + (c - 'a'))
+                                .sum();
+                    group.clear();
+                }
             }
             System.out.println(sum);
         }
