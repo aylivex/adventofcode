fcc01d70f8900483436b9d4026c8e47296bee4c8
Alexey Ivanov <code@alexey-ivanov.name>
2022-12-04 12:31:24 +0000

    Day 04 - First task

---
+++ src/Day04.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day04 {
+    public static void main(String[] args) throws IOException {
+        try (final BufferedReader r = new BufferedReader(
+                new FileReader("input/day04.input.txt"))) {
+            String line;
+            int sum = 0;
+            while ((line = r.readLine()) != null) {
+                List<Integer> intervals = Arrays.stream(line.split("[-,]"))
+                                                .map(Integer::valueOf)
+                                                .toList();
+                if ((intervals.get(0) <= intervals.get(2)
+                    && intervals.get(1) >= intervals.get(3))
+                    || (intervals.get(0) >= intervals.get(2)
+                       && intervals.get(1) <= intervals.get(3))) {
+                    sum++;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
