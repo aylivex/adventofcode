abc1bbe9caf705486abc972cf7dea57f08edbb4e
Alexey Ivanov <code@alexey-ivanov.name>
2022-12-10 00:30:21 +0000

    Day 09 - Second task

--- src/Day09.java
+++ src/Day09.java
@@ -3,23 +3,22 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 public class Day09 {
 
-    private static Point head;
-    private static Point tail;
-
-    private static Set<Point> visited;
-
     public static void main(String[] args) throws IOException {
-        visited = new HashSet<>();
+        Set<Point> visited = new HashSet<>();
 
-        head = new Point(0, 0);
-        tail = new Point(0, 0);
+        List<Point> rope = IntStream.range(0, 10)
+                                    .mapToObj(n -> new Point(0, 0))
+                                    .toList();
 
+        Point tail = rope.get(rope.size() - 1);
         visited.add(tail.getLocation());
 
         final Pattern pattern = Pattern.compile("([RULD]) (\\d+)");
@@ -36,8 +35,11 @@ public class Day09 {
                 final String direction = matcher.group(1);
                 int steps = Integer.parseInt(matcher.group(2));
                 while (steps-- > 0) {
-                    moveHead(direction);
-                    moveTail();
+                    moveHead(rope.get(0), direction);
+                    for (int i = 1; i < rope.size(); i++) {
+                        moveTail(rope.get(i - 1), rope.get(i));
+                    }
+                    visited.add(tail.getLocation());
                 }
             }
         }
@@ -45,7 +47,7 @@ public class Day09 {
         System.out.println(visited.size());
     }
 
-    private static void moveHead(String direction) {
+    private static void moveHead(Point head, String direction) {
         switch (direction) {
             case "R" -> head.x++;
             case "U" -> head.y++;
@@ -55,12 +57,14 @@ public class Day09 {
         }
     }
 
-    private static void moveTail() {
-        Point distance = getDistance();
+    private static void moveTail(Point head, Point tail) {
+        Point distance = getDistance(head, tail);
         if (isAdjacent(distance)) {
             // No need to move
             return;
-        } else if (Math.abs(distance.x) >= 2 && distance.y == 0) {
+        }
+
+        if (Math.abs(distance.x) >= 2 && distance.y == 0) {
             tail.x += sign(distance.x);
         } else if (distance.x == 0 && Math.abs(distance.y) >= 2) {
             tail.y += sign(distance.y);
@@ -68,14 +72,13 @@ public class Day09 {
             tail.x += sign(distance.x);
             tail.y += sign(distance.y);
         }
-        visited.add(tail.getLocation());
     }
 
     private static int sign(int n) {
         return n > 0 ? 1 : -1;
     }
 
-    private static Point getDistance() {
+    private static Point getDistance(Point head, Point tail) {
         return new Point(head.x - tail.x,
                          head.y - tail.y);
     }
