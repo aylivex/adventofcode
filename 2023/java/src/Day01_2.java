633e7256d68936e2e44af3e11f368eaba9f33fcf
Alexey Ivanov <code@alexey-ivanov.name>
2023-12-03 16:15:59 +0000

    2023: Day 1.2 - Get rid of CalibrationDigit record

--- 2023/java/src/Day01_2.java
+++ 2023/java/src/Day01_2.java
@@ -40,15 +40,13 @@ public class Day01_2 {
         System.out.println(calibrationValue);
     }
 
-    private record CalibrationDigit(int first, int index) {}
-
     private static int getCalibrationValue(final String line) {
-        CalibrationDigit first = getFirstCalibrationDigit(line);
-        CalibrationDigit last = getLastCalibrationDigit(line);
-        return first.first * 10 + last.first;
+        int first = getFirstCalibrationDigit(line);
+        int last = getLastCalibrationDigit(line);
+        return first * 10 + last;
     }
 
-    private static CalibrationDigit getFirstCalibrationDigit(final String line) {
+    private static int getFirstCalibrationDigit(final String line) {
         int[] spelt = Arrays.stream(SPELT_DIGITS.values())
                             .mapToInt(v -> line.indexOf(v.spelt))
                             .toArray();
@@ -62,30 +60,22 @@ public class Day01_2 {
         boolean dValid = dMinIndex >= 0;
 
         int first;
-        int nextIndex;
         if (sValid && dValid && spelt[sMinIndex] < digits[dMinIndex]) {
             first = SPELT_DIGITS.values()[sMinIndex].value;
-            nextIndex = spelt[sMinIndex]
-                        + SPELT_DIGITS.values()[sMinIndex].spelt.length();
         } else if (sValid && dValid) {
             first = DIGITS[dMinIndex].charAt(0) - '0';
-            nextIndex = digits[dMinIndex] + 1;
         } else if (sValid && !dValid) {
             first = SPELT_DIGITS.values()[sMinIndex].value;
-            nextIndex = spelt[sMinIndex]
-                        + SPELT_DIGITS.values()[sMinIndex].spelt.length();
         } else if (dValid) {
             first = DIGITS[dMinIndex].charAt(0) - '0';
-            nextIndex = digits[dMinIndex] + 1;
         } else {
             first = -1;
-            nextIndex = -1;
         }
 
-        return new CalibrationDigit(first, nextIndex);
+        return first;
     }
 
-    private static CalibrationDigit getLastCalibrationDigit(final String line) {
+    private static int getLastCalibrationDigit(final String line) {
         int[] spelt = Arrays.stream(SPELT_DIGITS.values())
                             .mapToInt(v -> line.lastIndexOf(v.spelt))
                             .toArray();
@@ -99,27 +89,19 @@ public class Day01_2 {
         boolean dValid = dMaxIndex >= 0;
 
         int first;
-        int nextIndex;
         if (sValid && dValid && spelt[sMaxIndex] > digits[dMaxIndex]) {
             first = SPELT_DIGITS.values()[sMaxIndex].value;
-            nextIndex = spelt[sMaxIndex]
-                    + SPELT_DIGITS.values()[sMaxIndex].spelt.length() + 1;
         } else if (sValid && dValid) {
             first = DIGITS[dMaxIndex].charAt(0) - '0';
-            nextIndex = digits[dMaxIndex] + 1;
         } else if (sValid && !dValid) {
             first = SPELT_DIGITS.values()[sMaxIndex].value;
-            nextIndex = spelt[sMaxIndex]
-                    + SPELT_DIGITS.values()[sMaxIndex].spelt.length() + 1;
         } else if (dValid) {
             first = DIGITS[dMaxIndex].charAt(0) - '0';
-            nextIndex = digits[dMaxIndex] + 1;
         } else {
             first = -1;
-            nextIndex = -1;
         }
 
-        return new CalibrationDigit(first, nextIndex);
+        return first;
     }
 
     private static int getMinValueIndex(final int[] array) {
