85a0d2ca8b72b3cbf0400e26119d23dc9e7eec12
Alexey Ivanov <code@alexey-ivanov.name>
2024-12-02 11:36:34 +0000

    Remove debug output and use real day02.txt

--- 2024/perl/Day02_1.pl
+++ 2024/perl/Day02_1.pl
@@ -2,7 +2,7 @@
 use warnings;
 use strict;
 
-open(INPUT, '<', "2024/input/sample.txt")
+open(INPUT, '<', "2024/input/day02.txt")
     or die $!;
 
 my $safe = 0;
@@ -10,34 +10,25 @@ my $safe = 0;
 input:
 while (<INPUT>) {
     my @levels = split /\s+/;
-    print "@levels\n";
 
     my $dir = 0;
     my $i = 0;
     do {{
         my $diff = $levels[$i] - $levels[$i + 1];
-        print "\t$diff ($dir)\n";
         if ($diff == 0) {
-            print "\t\t0\n";
             next input;
         }
         if (0 < $diff && $diff <= 3) {
-            print "\t\t 3\n";
             next input if ($dir < 0);
-            print "\t\t dir = 1\n";
             $dir = 1;
         } elsif (-3 <= $diff && $diff < 0) {
-            print "\t\t-3\n";
             next input if ($dir > 0);
-            print "\t\t dir = -1\n";
             $dir = -1;
         } else {
             next input;
         }
-        print "\t\t$dir\n";
     }} while (++$i < $#levels - 1);
 
-    print "\tsafe level\n";
     $safe++;
 }
 
