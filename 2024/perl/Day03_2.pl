bf6f708f3be8943eb42487efff2f74c9ae9fa9ba
Alexey Ivanov <code@alexey-ivanov.name>
2024-12-04 16:54:04 +0000

    Day 3, part 2: ‘enabled’ to be in outer while loop
    Remove debug traces too

--- 2024/perl/Day03_2.pl
+++ 2024/perl/Day03_2.pl
@@ -6,22 +6,18 @@ open(INPUT, '<', "2024/input/day03.txt")
     or die $!;
 
 my $result = 0;
+my $enabled = 1;
 
 while (<INPUT>) {
-    my $enabled = 1;
     my @intructions = /(do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\))/g;
     for my $instr (@intructions) {
-        print "$instr";
         $enabled = 1 if $instr eq "do()";
         $enabled = 0 if $instr eq "don't()";
-        print " - $enabled";
         if ($enabled) {
             if ($instr =~ /mul\((\d{1,3}),(\d{1,3})\)/) {
                 $result += ($1 * $2);
-                print " - $result ($1 * $2)";
             }
         }
-        print "\n"
     }
 }
 
